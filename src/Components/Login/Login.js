748effc6f21c1384492a069b5bbfa5b321e49581
rutuja.date <daterutuja5646@gmail.com>
2025-08-28 16:35:41 +0530

    login button and remaining css

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,32 @@
+import "./Login.css";
+
+function Login() {
+    return (
+        <div className="backgroundColor">
+            <div className="header">
+                <div className="blogsName">Blogs</div>
+                <div className="subHeader">
+                    <div className="loginName">Login</div>
+                    <div className="registerName">Register</div>
+                </div>
+            </div>
+            <div className="blogsBox">
+                <div className="blogs">Blogs</div>
+                <div className="blogsText">publish your passions,your way...</div>
+                <div className="loginText">Login</div>
+                <div className="emailId">Email id</div>
+                <div>
+                    <input type="text"
+                        placeholder="test@gmail.com" className="inputField" />
+                </div>
+                <div className="password">Password</div>
+                <div>
+                    <input type="Password"
+                        placeholder="test@123" className="inputField" />
+                </div>
+                <div><button className="loginButton">Login</button></div>
+            </div>
+        </div>
+    );
+}
+export default Login;
