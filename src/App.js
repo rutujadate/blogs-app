338c6f7cfbfd1bedaf4988e74bec6859031a3991
rutuja.date <daterutuja5646@gmail.com>
2025-08-29 16:36:53 +0530

    remaing textarea color and border color

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Login from './Components/Login/Login';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Register from './Components/Register/Register';
+import Title from './Components/Title/Title';
 
 function App() {
   return (
   // <div><Login/></div>
   // <div><Dashboard/></div>
-  <div><Register/></div>
+  // <div><Register/></div>
+  <div><Title/></div>
 
   );
 }
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,33 @@
+import "./Title.css";
+function Title(){
+    return(
+<div>
+    <div className="titleHeader">
+                <div className="titleBlogsName">Blogs</div>
+                <div className="titleSubHeader">
+                    <div className="titleLoginName">Rutuja Date</div>
+                    <div className="arrow">
+                        <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+
+                    </div>
+                    <div className="titleLogout">Logout</div>
+                </div>
+            </div>
+            <div className="textAreaBox">
+                <div>
+                    <input type="text"
+                    placeholder="Title" className="titleText"
+                    />
+                </div>
+                <textarea
+                type="text"
+                placeholder="Description" className="textAreaDescription"/>
+                <div className="textAreaButtons">
+                    <button className="buttons">Cancel</button>
+                    <button className="buttons">Save</button>
+                </div>
+            </div>
+</div>
+    );
+}
+export default Title;
